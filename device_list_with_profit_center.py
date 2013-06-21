c6ac62a8d50d74bd6d5c05d6cc58b75c5c75dbeb
Wojciech Matyśkiewicz <wojtek@matyskiewicz.com>
2013-06-21 12:17:25 +0200

    Update device_list_with_profit_center.py

--- device_list_with_profit_center.py
+++ device_list_with_profit_center.py
@@ -8,81 +8,47 @@ from __future__ import unicode_literals
 
 from ralph.discovery.models_device import Device
 
-import os, datetime, tempfile, smtplib, csv
-
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.MIMEBase import MIMEBase
-from email import Encoders
-
+import csv
 
 def dev_list():
     dev_list = [
-
+    
     ]
-    data = []
+    data = [
+        ['Name', 'Venture', 'Symbol', 'Role', 'Department', 'Profit center']
+    ]
     for hostname in dev_list:
-        device = Device.objects.get(name=hostname)
-        name = device.name
-        venture = device.venture.name
-        symbol = device.venture.symbol
-        role = device.role
-        department = None
-        if device.venture.department:
-            department = device.venture.department.name
-        profit_center = None
-        if device.venture.profit_center:
-            profit_center = device.venture.profit_center.name
-        data.append(
-            {
-                name,
-                venture,
-                symbol,
-                role,
-                department,
-                profit_center,
-            }
-        )
+
+        try:
+            device = Device.objects.filter(name=hostname)
+            for dev in device:
+                data.append(
+                    [
+                        dev.name,
+                        dev.venture.name,
+                        dev.venture.symbol,
+                        dev.role,
+                        dev.venture.department.name if dev.venture.department else None,
+                        dev.venture.profit_center.name if dev.venture.profit_center else None,
+                    ]
+                )
+        except Device.DoesNotExist:
+            data.append(
+                [
+                    hostname,
+                    'Does Not Exist',
+                    '',
+                    '',
+                    '',
+                    '',
+                ]
+            )
     return data
 
 
-def generate_temp_csv(csv_format):
-    filename = tempfile.mktemp(".csv")
-    f = open(filename, "wb")
-    writer = csv.writer(f, delimiter=b';', quotechar=b'"')
-    for row in csv_format:
-        unicode_row = [x for x in row]
-        # unicode_row = [x.encode('utf-8') for x in row]
-        writer.writerow(unicode_row)
-    f.close()
-    return f
+def generate_csv(csv_format):
+    c = csv.writer(open("MYFILE.csv", "wb"))
+    for item in csv_format:
+        c.writerow(item)
 
-
-def send_email(you, me='ralph@local', subject='Reports', text='', csv=None):
-    msg = MIMEMultipart()
-    msg['Subject'] = subject
-    msg['From'] = me
-    msg['To'] = you
-    msg.attach(MIMEText(text, 'plain'))
-    if csv is not None:
-        part = MIMEBase('application', "octet-stream")
-        part.set_payload(open(csv.name,"r").read())
-        Encoders.encode_base64(part)
-        part.add_header(
-            'Content-Disposition',
-            'attachment; filename="%s"' % os.path.basename(csv.name)
-        )
-        msg.attach(part)
-    send = smtplib.SMTP('localhost')
-    send.sendmail(me, you, msg.as_string())
-    send.quit()
-
-
-format_csv = dev_list()
-temp_csv = generate_temp_csv(format_csv)
-
-send_email(
-    you='wojciech.matyskiewic@ext.allegro.pl',
-    text="In attachment there is a file with report",
-    csv=temp_csv,
-)
+generate_csv(dev_list())
